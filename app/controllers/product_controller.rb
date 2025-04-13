a2f694ca3ee92af0757baa0ae8520564840746e5
Esteban Barbosa <esteban.barbosa2004@gmail.com>
2025-04-12 23:53:49 -0500

    Added products model; index, show, and new html pages; added home index html page

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,8 +1,28 @@
 class ProductController < ApplicationController
 
   def index
+    @products = Product.all
+    render :index
   end
 
   def show
+    @product = Product.find(params[:id])
+    render :show
+  end
+  
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(params.require(:product).permit(:name, :description, :price, :status, :image))
+    if @product.save
+      flash[:success] = 'New Product successfully added!'
+      redirect_to products_url
+    else
+      flash.now[:error] = 'Product Creation failed'
+      render :new, status: :unprocessable_entity
+    end
   end
 end

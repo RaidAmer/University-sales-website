5b76e15cdae07d0b9f7b282ee9e76f7eb4011bfb
Jimmy Nguyen <jtngyen5@memphis.edu>
2025-04-14 23:16:18 -0500

    Created the model class with its attributes and validations.

---
+++ app/models/event.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: events
+#
+#  id               :bigint           not null, primary key
+#  capacity         :integer
+#  date             :datetime
+#  description      :text
+#  event_name       :string
+#  image            :string
+#  location         :string
+#  price            :float
+#  registered_users :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+class Event < ApplicationRecord
+  serialize :registered_users, Array
+
+  validates :event_name, :location, :date, :price, :capacity, presence: true
+  validates :date, comparison: { greater_than_or_equal_to: Time.current }
+  validates :price, numericality: { greater_than_or_equal_to: 0.00 }
+  validates :capacity, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
+  has_one_attached :image
+end
---
+++ db/migrate/20250415040123_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :events do |t|
+      t.string :event_name
+      t.string :location
+      t.datetime :date
+      t.float :price
+      t.integer :capacity
+      t.text :registered_users
+      t.text :description
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
